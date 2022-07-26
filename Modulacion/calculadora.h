2489ecf5be4dd7b1c93b8989c15ef5341ecf62c6
Jasmin-Bonilla <106556008+Jasmin-Bonilla@users.noreply.github.com>
2022-07-26 13:50:11 -0600

    Add files via upload

---
+++ Modulacion/calculadora.h
@@ -0,0 +1,10 @@
+int suma(int a, int b);
+
+int restar(int a, int b);
+
+int multiplicar(int a, int b);
+
+int dividir(int a, int b);
+	
+
+
---
+++ Modulacion/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "calculadora.h"
+using namespace std;
+
+
+int main (int argc, char** argv)
+{
+	
+	
+	cout << suma(5, 7);
+	
+	return 0;
+}
+
+
+
